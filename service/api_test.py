2f4e80380b3aa56dd2180e655ebb226d21a85b5e
jinmaochen51@163.com <485s4850s!>
2020-12-03 09:22:33 +0800

    新增接口代码

---
+++ service/api_test.py
@@ -0,0 +1,14 @@
+import pytest
+import requests
+import json
+
+def test_product():
+    url = 'http://commdity-develop.kapeixi.cn/product/PPI1001001'
+    headers = {"content-type": "application/json"}
+    para = {'skuIdList': [773, 778, 788]}
+
+    r = requests.post(url, json=para, headers=headers)
+    print(json.dumps(r.json(),indent=2,ensure_ascii=False))
+
+if __name__ == '__main__':
+    pytest.main(["-vs","test_api.py"])
